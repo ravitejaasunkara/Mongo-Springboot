e1e4e9b027dc78f9c964051da0d3cadac7d4d14a
ravi <mooninthedarknight1999@gmail.com>
2024-12-09 23:22:15 +0530

    Added Save()

--- mongo/src/main/java/com/ravi/mongo/Controller/UserController.java
+++ mongo/src/main/java/com/ravi/mongo/Controller/UserController.java
@@ -4,10 +4,7 @@ package com.ravi.mongo.Controller;
 import com.ravi.mongo.Documents.User;
 import com.ravi.mongo.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -19,5 +16,10 @@ public class UserController {
     @GetMapping("/fetch/{userId}")
     public User getUser(@PathVariable("userId") String userId) {
         return userRepository.findById(userId).get();
+    }
+
+    @PostMapping("/save")
+    public void setUser(@RequestBody User user) {
+        userRepository.save(user);
     }
 }
